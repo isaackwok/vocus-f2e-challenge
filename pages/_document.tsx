05d3940b29ccae16db2b599c94aa879d75e6b9b8
Isaac Kwok <isaac.kwok@utonia.com>
2025-08-27 14:49:23 +0800

    feat: setup project with styles and configs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="zh-TW">
       <Head />
-      <body className="antialiased">
+      <body className="mx-auto max-w-3xl antialiased">
         <Main />
         <NextScript />
       </body>
